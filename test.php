7c308a92839ddc5b3dcfbc87e40d6ac4009a447f
Colin Tremblay <tremblay@grinnell.edu>
2013-11-13 22:01:28 -0600

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 include_once 'HTMLTable2JSON.php';
 $helper = new HTMLTable2JSON();
-$helper->tableToJSON('http://kdic.grinnell.edu/programs/schedule/', TRUE);
+$helper->tableToJSON('http://kdic.grinnell.edu/programs/schedule/');
 //$helper->tableToJSON('http://kdic.grinnell.edu/programs/schedule/', TRUE, 'wp-table-reloaded-id-6-no-1', array(0 => 5));
 //$helper->tableToJSON('http://lightswitch05.github.io/table-to-json/', FALSE);
 ?>
